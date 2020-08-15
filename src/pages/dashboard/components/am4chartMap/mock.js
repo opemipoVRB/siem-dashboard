ee448083fb442760402035560f10ed74b0e5aed1
opemipoVRB <opemipodurodola@gmail.com>
2020-08-15 16:42:25 +0100

    Initial commit

--- src/pages/dashboard/components/am4chartMap/mock.js
+++ src/pages/dashboard/components/am4chartMap/mock.js
@@ -1,28 +1,39 @@
 export default [
   {
-    latitude: 53.350140,
-    longitude: -6.266155,
+    latitude: 53.450140,
+    longitude: -6.264255,
     size: 6,
-    tooltip: 'Dublin',
+    tooltip: '79.97.106.65',
     fill: "#F00"
   },
-  {
-    latitude: 52.668018,
-    longitude: -8.630498,
-    size: 7,
-    tooltip: 'Limerick',
+{
+    latitude: 53.350140,
+    longitude: -6.266155,
+    size: 4,
+    tooltip: '192.141.28.11',
+    fill: "#F00"
   },
-  {
-    latitude: 51.903614,
-    longitude: -8.468399,
-    size: 10,
-    tooltip: 'Cork',
+{
+    latitude: 53.581788,
+    longitude: -6.266155,
+    size: 6,
+    tooltip: '185.40.67.12',
+    fill: "#F00"
   },
 
-  {
-    latitude: 	53.066643,
-    longitude: -8.818749,
-    size: 6,
-    tooltip: 'Galway',
-  }
+{
+    latitude: 53.557333,
+    longitude: -6.266155,
+    size: 8,
+    tooltip: '185.40.67.59',
+    fill: "#F00"
+  },
+    {
+    latitude: 53.575643,
+    longitude: -6.262345,
+    size: 8,
+    tooltip: '52.31.51.10',
+    fill: "#F00"
+  },
+
 ]
